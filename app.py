0825a6a56c7ca0822ef7f3570f7d728ace098743
Michael Fekadu <mike.fekadu@gmail.com>
2020-01-16 06:15:55 -0800

    test integration/config of heroku/github/etc

--- app.py
+++ app.py
@@ -4,9 +4,35 @@ import config
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello GroupMe-Bot!"
+
+
+@app.route("/post-groupme-message")
+def post_a_message():
+    pass
+
+
+def find_latest_message():
+    pass
+
+
+@app.route("/new-groupme-message")
+def get_new_message():
+    print("got a message!")
+    find_latest_message()
+    post_a_message()
+    # will remove this code on next commit, just curious to see
+    msg = "{}\n{}\n{}\n{}".format(
+        config.GROUPME_ACCESS_TOKEN,
+        config.GROUPME_CALLBACK_URL,
+        config.GROUPME_CLIENT_ID,
+        config.GROUPME_REDIRECT_URL
+    )
+    return msg
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
--- config.py
+++ config.py
@@ -1,8 +1,13 @@
-from os import environ as env
+from os import environ
 import multiprocessing
 
-PORT = int(env.get("PORT", 8080))
-DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
+PORT = int(environ.get("PORT", 8080))
+DEBUG_MODE = int(environ.get("DEBUG_MODE", 1))
+
+GROUPME_ACCESS_TOKEN = environ.get("GROUPME_ACCESS_TOKEN", None)
+GROUPME_CALLBACK_URL = environ.get("GROUPME_CALLBACK_URL", None)
+GROUPME_CLIENT_ID = environ.get("GROUPME_CLIENT_ID", None)
+GROUPME_REDIRECT_URL = environ.get("GROUPME_REDIRECT_URL", None)
 
 # Gunicorn config
 bind = ":" + str(PORT)
