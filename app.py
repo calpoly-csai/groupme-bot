2095471e708f993e3547322068a9faea12a82f8c
Michael Fekadu <mike.fekadu@gmail.com>
2020-01-16 07:56:09 -0800

    Fixes bugs on getting latest message, refactored a bit too

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 import config
 import requests
@@ -16,15 +16,8 @@ def post_a_message():
     pass
 
 
-def find_latest_message():
-    pass
-
-
-@app.route("/new-groupme-message")
-def got_new_message():
-    print("got a message!")
-
-    # Make the API URL
+def make_get_messages_by_group_id_url_params():
+    """Make the API URL"""
     base = "https://api.groupme.com/v3"
     base_grp = base + "/groups"
     base_grp_id = base_grp + "/{}"
@@ -33,16 +26,40 @@ def got_new_message():
         config.GROUPME_GROUP_ID,
     )
     PARAMS = {"token": config.GROUPME_ACCESS_TOKEN}
+    return URL, PARAMS
+
+
+def find_latest_message():
+    URL, PARAMS = make_get_messages_by_group_id_url_params()
     response = requests.get(url=URL, params=PARAMS)
     response_body = response.json()
+    return response_body.get('response', {}).get('messages', [None])[0]
 
-    # find_latest_message
-    latest_message = response_body.get('response').get('messages', [None])[0]
 
-    if latest_message is not None:
-        return latest_message, 200
+@app.route("/get-latest-message-object/", methods=['GET'])
+def get_latest_message_object():
+    latest_message = find_latest_message()
+
+    if latest_message is not None and type(latest_message) is dict:
+        return jsonify(latest_message), 200
     else:
-        return "latest_message is None... oops", 201
+        print("latest_message", latest_message)
+        return "latest_message is unexpected... oops", 201
+
+
+@app.route("/new-groupme-message")
+def got_new_message():
+    print("got a message!")
+
+    latest_message = find_latest_message()
+
+    if latest_message is not None and type(latest_message) == dict:
+        latest_message_text = latest_message.get("text", None)
+
+    if latest_message_text is not None:
+        return latest_message_text, 200
+    else:
+        return "latest_message_text is None... oops", 201
 
 
 if __name__ == "__main__":
